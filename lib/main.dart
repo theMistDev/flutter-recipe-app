6126d77d98efec723919c040c4c6b8ecfe952675
theMistDev <themistdev@gmail.com>
2023-04-01 15:09:15 +0100

    update

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/recipe_detail.dart';
+import 'recipe.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recipe Calculator',
+      theme: theme.copyWith(
+          colorScheme: theme.colorScheme
+              .copyWith(primary: Colors.grey, secondary: Colors.black)),
+      home: const MyHomePage(title: 'Recipe Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+          child: ListView.builder(
+              itemCount: Recipe.samples.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    }));
+                  },
+                  child: buildRecipeCard(Recipe.samples[index]),
+                );
+              })),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image(
+              image: AssetImage(recipe.imageUrl),
+            ),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'Palatino'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
